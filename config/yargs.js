5bef90de18c37f710d2a4a2e88204626c27847fe
Jonathan <jonathan.mc.96@hotmail.com>
2019-12-03 12:27:39 -0500

    Yargs

--- config/yargs.js
+++ config/yargs.js
@@ -1,27 +1,28 @@
 const descripcion = {
     demand: true,
-    alias: 'd',
+    alias: 'd', //Crea un alias para la descripción
     desc: "Descripción de la tarea por hacer"
 };
 
 const completado = {
     default: true,
-    alias: 'c',
+    alias: 'c', //Crea un alias para el estado de la tarea
     desc: "Marca como completada o pendiente la tarea"
 };
 
+//Definimos los comandos necesarios
 const argv = require('yargs')
-    .command('crear', 'Crear una tarea', {
+    .command('crear', 'Crear una tarea', { //comando crear, requiere la descripción de la tarea
         descripcion
     })
-    .command('actualizar', 'Actualiza una tarea', {
+    .command('actualizar', 'Actualiza una tarea', { //comando actualizar, requiere la descripción y el estado de la tarea 
         descripcion,
         completado
     })
-    .command('borrar', 'Elimina una tarea', {
+    .command('borrar', 'Elimina una tarea', { //comando borrar, requiere la descripción de la tarea
         descripcion
     })
-    .command('listar', 'Lista una tarea', {
+    .command('listar', 'Lista una tarea', { //comando listar, requiere el estado de la tarea
         completado
     })
     .help()
